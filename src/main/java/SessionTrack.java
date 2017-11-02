c582d05bdb8d6149a97432e5c3d570175f57f443
krimo <krimo.bensaid@gmail.com>
2017-11-02 21:13:19 +0100

    welcome to the area 51

--- src/main/java/SessionTrack.java
+++ src/main/java/SessionTrack.java
@@ -22,7 +22,7 @@ public class SessionTrack extends HttpServlet {
         Date lastAccessTime =
                 new Date(session.getLastAccessedTime());
 
-        String title = "Welcome Back to my website";
+        String title = "Welcome to the area 51";
         Integer visitCount = new Integer(0);
         String visitCountKey = new String("visitCount");
         String userIDKey = new String("userID");
